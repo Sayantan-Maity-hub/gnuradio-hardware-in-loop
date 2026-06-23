db6de1eab4796125eb0872382457a545c3f11a54
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-06-23 16:53:42 +0530

    Refactor main function in controller.py for improved readability

--- cortexlab/controller.py
+++ cortexlab/controller.py
@@ -2,3 +2,13 @@ import threading
 from reservation import reserve_nodes
 from scenario_generator import (create_task, generate_scenario, submit_task)
 from monitor import monitor_nodes
+
+def main():
+    print("\n Welcome to the cortexlab controller script")
+
+    #Reserve nodes via OAR resevation.py used here.
+    job_id, nodes = reserve_nodes()
+    print(job_id)
+    print(nodes)
+
+    
