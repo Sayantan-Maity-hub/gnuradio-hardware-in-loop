0add84d7610ed8f5dcc4bc457772bf2e6008e6df
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-06-23 15:59:24 +0530

    import reserve_node from recently written reservtion.py and scenario_generator function

--- cortexlab/controller.py
+++ cortexlab/controller.py
@@ -1,17 +1,3 @@
-import paramiko
-
-
-def get_node_info(hostname, username="root", port=2222):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(
-        paramiko.AutoAddPolicy()
-    )
-
-    ssh.connect(
-        hostname=hostname,
-        username=username,
-        port=port
-    )
-    stdin, stdout, stderr = ssh.exec_command("hostname")
-
-    node_hostname = stdout.read().decode().strip()
+import threading
+from reservation import reserve_nodes
+from scenario_generator import (create_task, generate_scenario, submit_task)
