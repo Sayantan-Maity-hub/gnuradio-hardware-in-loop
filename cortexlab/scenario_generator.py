1b32d1dbb4a49c6657723381fa347292b196961d
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-06-23 15:50:28 +0530

    Enhance generate_scenario function to build scenario nodes with container details

--- cortexlab/scenario_generator.py
+++ cortexlab/scenario_generator.py
@@ -5,3 +5,25 @@ def hostname_to_scenario_name(host):
 
 def create_task():
     subprocess.run("minus task create", shell=True, check=True)
+
+def generate_scenario(nodes):
+    scenario_nodes = []
+    for host in nodes:
+        scenario_name=(hostname_to_scenario_name(host))
+
+        scenario_nodes.extend([
+            f"{scenario_name}:",
+            "  container:",
+            "    -image: ghcr.io/cortex-lab/cxlb-gnuraio-3.10:1.5",
+            "     command: /usr/sbin/sshd -p 2222 -D",
+
+        ])
+        content="\n".join([
+            "description: Conroller and runner Communication Test",
+            "duration: 1200",
+            "",
+            "nodes:",
+            *scenario_nodes
+        ])
+
+        )
