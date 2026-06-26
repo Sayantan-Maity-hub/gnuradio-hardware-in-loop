eb18b3d925e99f4c0addedb668cbd890607eff19
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-06-26 15:27:24 +0530

    improve test in registry

--- cortexlab/registry.py
+++ cortexlab/registry.py
@@ -11,9 +11,11 @@ def get_nodes():
     
 # ======================TEST======================
 def test_registry():
+    print("\n[TEST] Registry update Test")
     update_node("node14", {"status": "ONLINE"})
 
     nodes = get_nodes()
     assert "node14" in nodes
     assert nodes["node14"]["status"] == "ONLINE"
+    print("[PASS] registry.py")
     
