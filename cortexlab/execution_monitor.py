811b008cb5bb3876dbf62d7260433f9ed66b3015
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-07-07 09:37:22 +0530

    change execution monitor to execution id based

--- cortexlab/execution_monitor.py
+++ cortexlab/execution_monitor.py
@@ -2,9 +2,19 @@ import threading
 import re
 import config
 from ssh_client import SSHConnection
-from execution_registy import(fail_execution, update_execution, finished_execution)
+from execution_registy import(get_execution, update_execution, fail_execution, finished_execution)
 
-def execute_script(node, folder, script, runner):
+def execute_script(execution_id):
+    execution = get_execution(execution_id)
+
+    if execution is None:
+        return
+    
+    node = execution["node"]
+    folder = execution["folder"]
+    script = execution["script"]
+    runner = execution["command"]
+
     ssh = SSHConnection(node)
     try :
         remote_script = f"/cortexlab/homes/{config.USERNAME}/{folder}/{script}"
@@ -24,10 +34,10 @@ def execute_script(node, folder, script, runner):
                 break
             print(line, end="")
             output += line
-            update_execution(node, stdout = output)
+            update_execution(execution_id, stdout = output)
             error = stderr.read().decode()
             if error:
-                update_execution(node, stderr=error)
+                update_execution(execution_id, stderr=error)
             exit_code = stdout.channel.recv_exit_status()
             result = "UNKWON"
             match = re.search(r"::STATUS:.*:(PASS|FAIL):", output)
@@ -38,8 +48,8 @@ def execute_script(node, folder, script, runner):
             else:
                 result = "FAILED"
 
-            finished_execution(node = node, result = result, stdout=output, stderr=error, log_path=log_path)
+            finished_execution(execution_id=execution_id, result = result, stdout=output, stderr=error, log_path=log_path)
     except Exception as e:
-        fail_execution(node, error=str(e))
+        fail_execution(execution_id, error=str(e))
     finally:
         ssh.close()
